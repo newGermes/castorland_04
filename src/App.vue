a63b2d72c6ee6c63a575a67542ae572741d791d7
newGermes <kukhar.vitalii@gmail.com>
2018-01-26 19:20:29 +0200

    intermediate results

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <header class="header">
+      <nav class="inner">
+        <router-link to="/" exact>
+          <img class="logo" src="" alt="logo">
+        </router-link>
+
+      </nav>
+    </header>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<style lang="sass">
+ 
+</style>
